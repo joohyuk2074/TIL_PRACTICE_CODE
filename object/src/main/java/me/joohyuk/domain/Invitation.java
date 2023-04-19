e5fef02d6b2c154dff21ab25706eb1a3ec3819fb
joohyuk2074 <vfrvfr4207@gmail.com>
2023-04-20 02:05:07 +0900

    feat: Chapter 01 애플리케이션 준비 완료

---
+++ object/src/main/java/me/joohyuk/domain/Invitation.java
@@ -0,0 +1,8 @@
+package me.joohyuk.domain;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+
+    private LocalDateTime when;
+}
---
+++ object/src/main/java/me/joohyuk/domain/Ticket.java
@@ -0,0 +1,10 @@
+package me.joohyuk.domain;
+
+public class Ticket {
+
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
