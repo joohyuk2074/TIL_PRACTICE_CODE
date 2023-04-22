8529518ca7fd62b452b2d4c19cfa5fbd06ab176f
joohyuk2074 <vfrvfr4207@gmail.com>
2023-04-23 00:49:00 +0900

    feat: Chapter 02. 객체지향 프로그래밍

---
+++ object/src/main/java/me/joohyuk/Chapter02/Customer.java
@@ -0,0 +1,4 @@
+package me.joohyuk.Chapter02;
+
+public class Customer {
+}
---
+++ object/src/main/java/me/joohyuk/Chapter02/NoneDefaultDiscountPolicy.java
@@ -0,0 +1,9 @@
+package me.joohyuk.Chapter02;
+
+public class NoneDefaultDiscountPolicy implements DiscountPolicy {
+
+    @Override
+    public Money calculateDiscountAmount(Screening screening) {
+        return Money.ZERO;
+    }
+}
