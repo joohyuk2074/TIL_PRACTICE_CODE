24d856a8b8718c4361637414f7b6be215ce48b03
joohyuk2074 <vfrvfr4207@gmail.com>
2023-04-26 00:43:27 +0900

    feat: Chapater 04. Before

---
+++ object/src/main/java/me/joohyuk/Chapter04/Movie.java
@@ -0,0 +1,69 @@
+package me.joohyuk.Chapter04;
+
+public class Movie {
+
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MonvieType monvieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Duration getRunningTime() {
+        return runningTime;
+    }
+
+    public void setRunningTime(Duration runningTime) {
+        this.runningTime = runningTime;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return discountConditions;
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public MonvieType getMonvieType() {
+        return monvieType;
+    }
+
+    public void setMonvieType(MonvieType monvieType) {
+        this.monvieType = monvieType;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+}
