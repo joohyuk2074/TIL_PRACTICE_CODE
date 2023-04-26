d1c9e5e6df22d8c3bc40688479d6f0a2dfefc637
joohyuk2074 <vfrvfr4207@gmail.com>
2023-04-26 15:05:51 +0900

    feat: tokenize 환경 설정

--- tokenizer/src/main/java/me/joohyuk/Main.java
+++
@@ -1,7 +0,0 @@
-package me.joohyuk;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ tokenizer/src/main/java/me/joohyuk/tokenizer/TokenizerApplication.java
@@ -0,0 +1,11 @@
+package me.joohyuk.tokenizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenizerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TokenizerApplication.class, args);
+    }
+}
