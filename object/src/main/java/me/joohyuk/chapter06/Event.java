ba5d5ffba29de4d03d9db796d15b7cfdef20bd6b
joohyuk2074 <vfrvfr4207@gmail.com>
2023-04-28 01:18:10 +0900

    feat: Chapter 06 실습 코드 추가

---
+++ object/src/main/java/me/joohyuk/chapter06/Event.java
@@ -0,0 +1,30 @@
+package me.joohyuk.chapter06;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Event {
+
+    private final String subject;
+    private final LocalDateTime from;
+    private final Duration duration;
+
+    public Event(String subject, LocalDateTime from, Duration duration) {
+        this.subject = subject;
+        this.from = from;
+        this.duration = duration;
+    }
+
+    public boolean isSatisfied(RecurringSchedule schedule) {
+        if (from.getDayOfWeek() != schedule.getDayOfWeek() ||
+            !from.toLocalTime().equals(schedule.getFrom()) ||
+            !duration.equals(schedule.getDuration())) {
+            return false;
+        }
+        return true;
+    }
+
+    public void reschedule(RecurringSchedule schedule) {
+        from = LocalDateTime.of(from.toLocalDate().plusDays(daysDistance(schedule)), schedule.getFrom());
+    }
+}
