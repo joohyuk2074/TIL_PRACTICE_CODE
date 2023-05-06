9f1b7bfdf4cde67d1ae65a13f143c6099cf8d71e
joohyuk2074 <vfrvfr4207@gmail.com>
2023-05-06 21:17:13 +0900

    feat: DFS/BFS, 정렬

---
+++ python-algorithm/thisIsCondingTest/graph/search.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end='')
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end='')
+
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
