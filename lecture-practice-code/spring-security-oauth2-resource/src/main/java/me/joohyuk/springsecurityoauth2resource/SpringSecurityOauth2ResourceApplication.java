56348f35531e2cdf8287f9a7eaf085dade3c155b
joohyuk2074 <vfrvfr4207@gmail.com>
2023-05-10 01:12:25 +0900

    feat: OAuth 2.0 Resource Server 소개 및 프로젝트 생성

---
+++ lecture-practice-code/spring-security-oauth2-resource/src/main/java/me/joohyuk/springsecurityoauth2resource/SpringSecurityOauth2ResourceApplication.java
@@ -0,0 +1,13 @@
+package me.joohyuk.springsecurityoauth2resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2ResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2ResourceApplication.class, args);
+    }
+
+}
