3de64ca1a8d1f4d67960ed43c8015dcc4dcb3669
joohyuk2074 <vfrvfr4207@gmail.com>
2023-05-11 01:50:06 +0900

    feat: Implementing POST Method to create User Resource

--- lecture-practice-code/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ lecture-practice-code/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,28 +1,17 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class UserResource {
-    private UserDaoService service;
 
-    public UserResource(UserDaoService service) {
-        this.service = service;
-    }
+    //SAME_OLD_CODE
 
-    // GET /users
-    @GetMapping("/users")
-    public List<User> retrieveAllUsers() {
-        return service.findAll();
-    }
-
-    // GET /users
-    @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+    //POST /users
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        service.save(user);
     }
 }
