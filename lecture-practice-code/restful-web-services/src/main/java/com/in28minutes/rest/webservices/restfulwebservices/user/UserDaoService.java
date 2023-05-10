fb34b0053b88c63f9817a2fd291cb5a3132390d4
joohyuk2074 <vfrvfr4207@gmail.com>
2023-05-11 01:48:08 +0900

    feat: Implementing GET Methods for User Resource

--- lecture-practice-code/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ lecture-practice-code/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -5,16 +5,17 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
-    // JPA/Hibernate > Database
+// JPA/Hibernate > Database
     // UserDaoService > Static List
 
     private static List<User> users = new ArrayList<>();
 
     static {
-        users.add(new User(1,"Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(1,"Adam",LocalDate.now().minusYears(30)));
         users.add(new User(2,"Eve",LocalDate.now().minusYears(25)));
         users.add(new User(3,"Jim",LocalDate.now().minusYears(20)));
     }
@@ -25,5 +26,8 @@ public class UserDaoService {
 
     //public User save(User user) {
 
-    //public User findOne(int id) {
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
+    }
 }
