16cdf9204667ca89b039b768f2184764938d6a4f
joohyuk2074 <vfrvfr4207@gmail.com>
2023-05-14 17:15:11 +0900

    feat: 알고리즘 문제 풀이

--- python-algorithm/Main01.py
+++ python-algorithm/Main01.py
@@ -1,1 +1,14 @@
-print("hello")
+def solution(arr):
+    arr_length = len(arr)
+    if arr_length == 1:
+        return arr
+
+    q = 1
+    while 2 ** q < arr_length:
+        q += 1
+
+    length = 2 ** q
+    for i in range(0, length - arr_length):
+        arr.append(0)
+
+    return arr
