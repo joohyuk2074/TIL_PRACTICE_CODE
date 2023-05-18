d864894c53570ad21f641c38026c38c2a2dd8997
joohyuk2074 <vfrvfr4207@gmail.com>
2023-05-19 00:20:52 +0900

    feat: DefaultOAuth2AuthorizedClientManager 기본 환경 구성

---
+++ lecture-practice-code/spring-security-oauth2-client/src/main/java/me/joohyuk/springsecurityoauth2client/config/OAuth2Config.java
@@ -0,0 +1,24 @@
+package me.joohyuk.springsecurityoauth2client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class OAuth2Config {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authRequest -> authRequest
+            .requestMatchers("/", "oauth2Login", "logout", "client").permitAll()
+            .anyRequest().authenticated());
+
+        http
+            .oauth2Client();
+
+        return http.build();
+    }
+}
