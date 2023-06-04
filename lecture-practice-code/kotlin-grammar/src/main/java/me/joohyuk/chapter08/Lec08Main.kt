422cfcc5b7ed20aaf33dca2420414770967c33d1
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-04 17:36:28 +0900

    feat: Chapter 06. 코틀린에서 함수를 다루는 방법

---
+++ lecture-practice-code/kotlin-grammar/src/main/java/me/joohyuk/chapter08/Lec08Main.kt
@@ -0,0 +1,31 @@
+package me.joohyuk.chapter08
+
+fun main() {
+    repeat("Hello World", useNewLine = true)
+
+    printAll("A", "B", "C")
+    val array = arrayOf("A", "B", "C")
+    printAll(*array)
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+fun repeat(
+    str: String,
+    num: Int = 3,
+    useNewLine: Boolean = true
+) {
+    for (i in 1..num) {
+        if (useNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
+
+fun printAll(vararg strings: String) {
+    for (str in strings) {
+        println(str)
+    }
+}
