ef7952b535b8a6fa1842c6421a693f1060be651f
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-04 19:56:50 +0900

    feat: 최소직사각형

--- python-algorithm/programmers/level1/최소직각사각형.py
+++ python-algorithm/programmers/level1/최소직각사각형.py
@@ -1,10 +1,22 @@
 def solution(sizes):
     answer = 0
-    #
-    # max_width = 0
-    # max_height = 0
-    #
-    # for width, height in sizes:
-    #
-    #
-    return answer
+
+    widths = []
+    heights = []
+
+    for width, height in sizes:
+        if width < height:
+            widths.append(height)
+            heights.append(width)
+        else:
+            widths.append(width)
+            heights.append(height)
+
+    w_max = max(widths)
+    h_max = max(heights)
+
+    return w_max * h_max
+
+
+def other_solution(sizes):
+    return max(max(x) for x in sizes * max(min(x) for x in sizes))
