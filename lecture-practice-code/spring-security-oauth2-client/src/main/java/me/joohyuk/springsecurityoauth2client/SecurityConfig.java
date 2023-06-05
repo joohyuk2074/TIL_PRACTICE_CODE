7fc2a3e8d5ac09c3af09af0d077b64216ce45413
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-05 17:30:49 +0900

    feat: 자동설정에 의한 초기화 과정 이해

--- lecture-practice-code/spring-security-oauth2-client/src/main/java/me/joohyuk/springsecurityoauth2client/SecurityConfig.java
+++ lecture-practice-code/spring-security-oauth2-client/src/main/java/me/joohyuk/springsecurityoauth2client/SecurityConfig.java
@@ -1,18 +1,25 @@
 package me.joohyuk.springsecurityoauth2client;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().anyRequest().authenticated();
         http.formLogin();
-        http.apply(new CustomSecurityConfigurer().setFlag(false));
+//        http.apply(new CustomSecurityConfigurer().setFlag(false));
+        return http.build();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.httpBasic();
         return http.build();
     }
 }
