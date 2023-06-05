0246084297381adbaf7df7229e3bc9d3eabefa0e
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-05 16:49:58 +0900

    feat: 초기화 과정 이해 - SecurityBuilder / SecurityConfigurer

---
+++ lecture-practice-code/spring-security-oauth2-client/src/main/java/me/joohyuk/springsecurityoauth2client/CustomSecurityConfigurer.java
@@ -0,0 +1,33 @@
+package me.joohyuk.springsecurityoauth2client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Slf4j
+public class CustomSecurityConfigurer extends AbstractHttpConfigurer<CustomSecurityConfigurer, HttpSecurity> {
+
+    private boolean isSecure;
+
+    @Override
+    public void init(HttpSecurity builder) throws Exception {
+        super.init(builder);
+        log.info("init method started..");
+    }
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        super.configure(builder);
+        log.info("configure method started..");
+        if (isSecure) {
+            log.info("https is required");
+        } else {
+            log.info("http is optional");
+        }
+    }
+
+    public CustomSecurityConfigurer setFlag(boolean isSecure) {
+        this.isSecure = isSecure;
+        return this;
+    }
+}
