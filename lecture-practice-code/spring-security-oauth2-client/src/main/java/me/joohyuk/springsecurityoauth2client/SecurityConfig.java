e95a7b0493490e604a58fb11bbd7f8959c526dfd
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-06 15:05:08 +0900

    feat: Http Basic 인증

--- lecture-practice-code/spring-security-oauth2-client/src/main/java/me/joohyuk/springsecurityoauth2client/SecurityConfig.java
+++ lecture-practice-code/spring-security-oauth2-client/src/main/java/me/joohyuk/springsecurityoauth2client/SecurityConfig.java
@@ -1,19 +1,19 @@
 package me.joohyuk.springsecurityoauth2client;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().anyRequest().authenticated();
-        http.formLogin();
-        http.httpBasic();
-        http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> System.out.println("Custom entryPoint"));
+        http.httpBasic().authenticationEntryPoint(new CustomerAuthenticationEntryPoint());
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 }
