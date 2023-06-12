34b7a923daa8a75be736b021847956cb12346cb6
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-12 18:30:01 +0900

    feat: ClientRegistrationRepository 이해 및 활용

--- lecture-practice-code/spring-security-oauth2-client/src/main/java/me/joohyuk/springsecurityoauth2client/IndexController.java
+++ lecture-practice-code/spring-security-oauth2-client/src/main/java/me/joohyuk/springsecurityoauth2client/IndexController.java
@@ -1,13 +1,27 @@
 package me.joohyuk.springsecurityoauth2client;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class IndexController {
 
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
     @GetMapping("/")
     public String index() {
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+
+        String clientId = clientRegistration.getClientId();
+        System.out.println("clientId: " + clientId);
+
+        String registrationId = clientRegistration.getRegistrationId();
+        System.out.println("registrationId: " + registrationId);
+
         return "index";
     }
 }
