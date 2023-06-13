28d7aaa93086f60261732053d9f9930cb44d812a
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-13 23:40:24 +0900

    feat: n^2 배열 자르기

--- python-algorithm/programmers/level2/n^2 배열 자르기.py
+++ python-algorithm/programmers/level2/n^2 배열 자르기.py
@@ -1,3 +1,32 @@
 def solution(n, left, right):
     answer = []
+
+    start_row = left // n
+    end_row = right // n
+
+    for i in range(start_row, end_row + 1):
+        if i == start_row:
+            start_col = left % n
+        else:
+            start_col = 0
+
+        if i == end_row:
+            end_col = right % n
+        else:
+            end_col = n - 1
+
+        for j in range(start_col, end_col + 1):
+            index = max(i, j)
+            answer.append(index + 1)
+
     return answer
+
+
+def other_solution(n, left, right):
+    answer = []
+    for i in range(left, right + 1):
+        answer.append(max(i // n, i % n) + 1)
+    return answer
+
+
+print(solution(4, 7, 14))
