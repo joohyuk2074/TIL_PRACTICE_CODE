1b6cddb37130826b639b11616083de8f3a0afc10
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-17 00:11:54 +0900

    feat: Chapter 02. 안녕! 스프링 시큐리티

---
+++ book-practice-code/spring-security-in-action/ssia-ch2/src/main/java/me/joohyuk/ssiach2/SsiaCh2Application.java
@@ -0,0 +1,13 @@
+package me.joohyuk.ssiach2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh2Application.class, args);
+    }
+
+}
---
+++ book-practice-code/spring-security-in-action/ssia-ch2/src/test/java/me/joohyuk/ssiach2/SsiaCh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joohyuk.ssiach2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
