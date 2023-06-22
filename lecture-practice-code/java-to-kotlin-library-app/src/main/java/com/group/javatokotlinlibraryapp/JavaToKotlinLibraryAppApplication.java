7ef6d5b0dfe96bf2de3e6bae5cb61d965f77cd4f
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-22 22:28:54 +0900

    feat: 프로젝트 초기화

---
+++ lecture-practice-code/java-to-kotlin-library-app/src/main/java/com/group/javatokotlinlibraryapp/JavaToKotlinLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.group.javatokotlinlibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToKotlinLibraryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaToKotlinLibraryAppApplication.class, args);
+    }
+
+}
