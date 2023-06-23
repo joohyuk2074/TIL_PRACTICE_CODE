ca8b241d34c37d92c938546c73fdef4e522ac765
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-24 01:00:19 +0900

    feat: 사칙연산 계산기에 대해 테스트 코드 작성하기

---
+++ lecture-practice-code/java-to-kotlin-library-app/src/test/kotlin/com/group/javatokotlinlibraryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,51 @@
+package com.group.javatokotlinlibraryapp.calculator
+
+fun main() {
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+}
+
+class CalculatorTest {
+
+    fun addTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.add(3)
+
+        // then
+        if (calculator.number != 8) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        if (calculator.number != 15) {
+            throw IllegalArgumentException()
+        }
+    }
+
+}
