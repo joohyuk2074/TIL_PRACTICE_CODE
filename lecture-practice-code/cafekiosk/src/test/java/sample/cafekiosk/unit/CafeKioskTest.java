af785dd31a85109155b0d8da3f03a0560f21e2cf
joohyuk2074 <vfrvfr4207@gmail.com>
2023-06-27 19:44:43 +0900

    feat: BDD 스타일로 작성하기

--- lecture-practice-code/cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ lecture-practice-code/cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -81,16 +81,20 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).hasSize(0);
     }
 
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice() {
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
 
+        // when
         int totalPrice = cafeKiosk.calculateTotalPrice();
 
+        // then
         assertThat(totalPrice).isEqualTo(8500);
     }
 
