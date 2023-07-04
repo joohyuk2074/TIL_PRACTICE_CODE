6175dc464222e116de7a8242e3af0104a9e12a03
joohyuk2074 <vfrvfr4207@gmail.com>
2023-07-04 17:14:16 +0900

    feat: Presentation Layer 테스트 (2)

--- lecture-practice-code/cafekiosk/src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ lecture-practice-code/cafekiosk/src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -1,10 +1,13 @@
 package sample.cafekiosk.spring.api.controller.product;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.ApiResponse;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
@@ -18,12 +21,14 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public void createProduct(@RequestBody ProductCreateRequest productCreateRequest) {
-        productService.createProduct(productCreateRequest);
+    public ApiResponse<ProductResponse> createProduct(@Valid @RequestBody ProductCreateRequest productCreateRequest) {
+        ProductResponse productResponse = productService.createProduct(productCreateRequest.toServiceRequest());
+        return ApiResponse.of(HttpStatus.OK, productResponse);
     }
 
     @GetMapping("/api/v1/products/selling")
-    public List<ProductResponse> getSellingProducts() {
-        return productService.getSellingProducts();
+    public ApiResponse<List<ProductResponse>> getSellingProducts() {
+        List<ProductResponse> sellingProducts = productService.getSellingProducts();
+        return ApiResponse.ok(sellingProducts);
     }
 }
--- lecture-practice-code/cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/request/ProductCreateServiceRequest.java
+++ lecture-practice-code/cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/request/ProductCreateServiceRequest.java
@@ -1,4 +1,4 @@
-package sample.cafekiosk.spring.api.controller.product.dto.request;
+package sample.cafekiosk.spring.api.service.product.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,38 +7,33 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import sample.cafekiosk.spring.api.service.product.request.ProductCreateServiceRequest;
+import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ProductCreateRequest {
+public class ProductCreateServiceRequest {
 
-    @NotNull(message = "상품 타입은 필수입니다.")
     private ProductType type;
 
-    @NotNull(message = "상품 판매상태는 필수입니다.")
     private ProductSellingStatus sellingStatus;
 
-    // String name -> 상품 이름은 20자 제한을 여기서 검증하는게 맞나? Product 생성시점에서 검증하는게 맞는듯
-//    @Max(20)
-    @NotBlank(message = "상품 이름은 필수입니다.")
     private String name;
 
-    @Positive(message = "상품 가격은 양수여야 합니다.")
     private int price;
 
     @Builder
-    private ProductCreateRequest(ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+    private ProductCreateServiceRequest(ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
         this.type = type;
         this.sellingStatus = sellingStatus;
         this.name = name;
         this.price = price;
     }
 
-    public ProductCreateServiceRequest toServiceRequest() {
-        return ProductCreateServiceRequest.builder()
+    public Product toEntity(String nextProductNumber) {
+        return Product.builder()
+            .productNumber(nextProductNumber)
             .type(type)
             .sellingStatus(sellingStatus)
             .name(name)
