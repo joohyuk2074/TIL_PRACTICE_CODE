bfa4be1b63927e32016e81a9a664d9d4c771d49c
joohyuk2074 <vfrvfr4207@gmail.com>
2023-07-08 00:31:03 +0900

    feat: 13강. 도메인 계층을 Kotlin으로 변경하기 - UserLoanHistory.java, User.java

---
+++ lecture-practice-code/java-to-kotlin-library-app/src/main/kotlin/com/group/javatokotlinlibraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -0,0 +1,22 @@
+package com.group.javatokotlinlibraryapp.domain.user.loanhistory
+
+import com.group.javatokotlinlibraryapp.domain.user.User
+import jakarta.persistence.*
+
+@Entity
+class UserLoanHistory(
+    @ManyToOne
+    val user: User,
+
+    val bookName: String,
+
+    var isReturn: Boolean,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+) {
+    fun doReturn() {
+        this.isReturn = true
+    }
+}
