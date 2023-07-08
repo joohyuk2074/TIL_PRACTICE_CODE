54e90387b5aa5c655fdacfd59dbff8d52c11ae5e
joohyuk2074 <vfrvfr4207@gmail.com>
2023-07-09 02:00:47 +0900

    feat: 피로도 풀이

---
+++ python-algorithm/thisIsCondingTest/graph/dfs예제.py
@@ -0,0 +1,8 @@
+def dfs_with_recursive(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs_with_recursive(graph, i, visited)
