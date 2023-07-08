896fa60586e712f0097a55957e1c6dfc564cf0c6
joohyuk2074 <vfrvfr4207@gmail.com>
2023-07-08 20:01:33 +0900

    feat: 큰 수 법칙 다시 풀이

--- python-algorithm/thisIsCondingTest/greedy/큰 수의 법칙(단순풀이).py
+++ python-algorithm/thisIsCondingTest/greedy/큰 수의 법칙(단순풀이).py
@@ -1,19 +1,26 @@
+def solution(n, m, k, data):
+    answer = 0
+
+    data.sort()
+    first_num = data[n - 1]
+    second_num = data[n - 2]
+
+    while m > 0:
+        cnt = k
+        while cnt > 0 and m > 0:
+            answer += first_num
+            m -= 1
+            cnt -= 1
+
+        if cnt <= 0 and m > 0:
+            answer += second_num
+            m -= 1
+            cnt = k
+
+    return answer
+
+
 n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
 
-data.sort()
-first = data[n - 1]
-second = data[n - 2]
-
-result = 0
-
-while True:
-    for i in range(k):
-        if m == 0:
-            break
-        result += first
-        m -= 1
-    if m == 0:
-        break
-    result += second
-    m -= 1
+print(solution(n, m, k, data))
