79d93da24a7b13cdf98960d3d26e426ed1fa9d43
joohyuk2074 <vfrvfr4207@gmail.com>
2023-07-09 15:17:39 +0900

    feat: 정렬

--- python-algorithm/thisIsCondingTest/sort/quick_sort.py
+++ python-algorithm/thisIsCondingTest/sort/quick_sort.py
@@ -2,24 +2,40 @@ array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
 
 def quick_sort(array, start, end):
-    if start >= end:  # 원소가 1개인 경우
+    if start >= end:  # 원소가 1개인 경우 종료
         return
 
-    pivot = start
+    pivot = start  # 피벗은 첫 번째 원소
     left = start + 1
     right = end
     while left <= right:
+        # 피벗보다 큰 데이터를 찾을 때까지 반복
         while left <= end and array[left] <= array[pivot]:
             left += 1
+        # 피벗보다 작은 데이터를 찾을 때까지 반복
         while right > start and array[right] >= array[pivot]:
             right -= 1
-        if left > right:
+        if left > right:  # 엇갈렸다면 작은 right -= 1 데이터와 피벗을 교체
             array[right], array[pivot] = array[pivot], array[right]
         else:
             array[left], array[right] = array[right], array[left]
 
     quick_sort(array, start, right - 1)
     quick_sort(array, right + 1, end)
+
+
+def quick_sort2(array):
+    # 리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0]  # 피벗은 첫 번째 원소
+    tail = array[1:0]  # 피벗을 제외한 리스트
+
+    left_side = [x for x in tail if x <= pivot]  # 분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot]  # 분할된 오른쪽 부분
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
 
 
 quick_sort(array, 0, len(array) - 1)
