b10af5cb89e43859f459434155cb88bd64d39e73
joohyuk2074 <vfrvfr4207@gmail.com>
2023-07-12 00:37:59 +0900

    feat: 17강. BookService.java를 Kotlin으로 변경하고 Optional 제거하기

--- lecture-practice-code/java-to-kotlin-library-app/src/main/kotlin/com/group/javatokotlinlibraryapp/service/user/UserService.kt
+++ lecture-practice-code/java-to-kotlin-library-app/src/main/kotlin/com/group/javatokotlinlibraryapp/service/user/UserService.kt
@@ -5,6 +5,8 @@ import com.group.javatokotlinlibraryapp.domain.user.UserRepository
 import com.group.javatokotlinlibraryapp.dto.user.request.UserCreateRequest
 import com.group.javatokotlinlibraryapp.dto.user.request.UserUpdateRequest
 import com.group.javatokotlinlibraryapp.dto.user.response.UserResponse
+import com.group.javatokotlinlibraryapp.util.fail
+import com.group.javatokotlinlibraryapp.util.findByIdOrThrow
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -27,13 +29,13 @@ class UserService(
 
     @Transactional
     fun updateUserName(request: UserUpdateRequest) {
-        val user = userRepository.findById(request.id).orElseThrow(::IllegalArgumentException)
+        val user = userRepository.findByIdOrThrow(request.id)
         user.updateName(request.name)
     }
 
     @Transactional
     fun deleteUser(name: String) {
-        val user = userRepository.findByName(name).orElseThrow(::IllegalArgumentException)
+        val user = userRepository.findByName(name) ?: fail()
         userRepository.delete(user)
     }
 }
