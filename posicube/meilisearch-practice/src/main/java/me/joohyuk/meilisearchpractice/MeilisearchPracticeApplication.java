016eff37122f9c9141ae540f3943f5a18680a5a2
joohyuk2074 <vfrvfr4207@gmail.com>
2023-08-04 13:57:22 +0900

    feat: meilisearch 학습 프로젝트 생성

---
+++ posicube/meilisearch-practice/src/main/java/me/joohyuk/meilisearchpractice/MeilisearchPracticeApplication.java
@@ -0,0 +1,13 @@
+package me.joohyuk.meilisearchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeilisearchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeilisearchPracticeApplication.class, args);
+    }
+
+}
---
+++ posicube/meilisearch-practice/src/test/java/me/joohyuk/meilisearchpractice/MeilisearchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joohyuk.meilisearchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeilisearchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
