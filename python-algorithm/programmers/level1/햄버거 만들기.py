28186407d2ad953b8f592ab05f9257890623c5c6
joohyuk2074 <vfrvfr4207@gmail.com>
2023-08-13 18:41:01 +0900

    feat: 햄버거 만들기

---
+++ python-algorithm/programmers/level1/햄버거 만들기.py
@@ -0,0 +1,15 @@
+def solution(ingredient):
+    s = []
+    cnt = 0
+    for i in ingredient:
+        s.append(i)
+        if s[-4:] == [1, 2, 3, 1]:
+            cnt += 1
+            for i in range(4):
+                s.pop()
+
+    return cnt
+
+
+ingredient = [2, 1, 1, 2, 3, 1, 2, 3, 1]
+print(solution(ingredient))
