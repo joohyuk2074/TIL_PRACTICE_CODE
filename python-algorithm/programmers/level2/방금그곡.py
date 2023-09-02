7ae0ddc00d231953940f21b4c371a647f1c0cab4
joohyuk2074 <vfrvfr4207@gmail.com>
2023-09-02 15:19:43 +0900

    방금 그곡: 6, 20, 21, 30 실패

--- python-algorithm/programmers/level2/방금그곡.py
+++ python-algorithm/programmers/level2/방금그곡.py
@@ -1,59 +1,73 @@
 def solution(m, musicinfos):
     answer = ''
 
-    music_name_to_orders = {}
-    global_orders = {}
-    for musicinfo in musicinfos:
-        split = musicinfo.split(",")
-        start_time = get_time_to_minute(split[0])
-        end_time = get_time_to_minute(split[1])
+    memory_orders = replace_orders(m)
 
-        orders = get_list_order(split[3])
+    music_orders_str = ''
+    music_order_list = []
+
+    for music_info in musicinfos:
+        split = music_info.split(",")
+
+        start_minute = time_to_long(split[0])
+        end_minute = time_to_long(split[1])
         music_name = split[2]
+        music_orders = replace_orders(split[3])
 
-        music_name_to_orders[music_name] = orders
-        global_orders[music_name] = []
-
-        index = 0
-        duration = end_time - start_time
-        while duration >= 0:
-            global_orders[music_name].append(orders[index])
-
-            if index == len(orders) - 1:
-                index = 0
-            else:
-                index += 1
-
+        music_order_idx = 0
+        duration = end_minute - start_minute
+        while duration > 0:
+            music_orders_str += music_orders[music_order_idx]
+            music_order_list.append((music_orders[music_order_idx], music_name))
             duration -= 1
 
+            music_order_idx += 1
+            if music_order_idx == len(music_orders):
+                music_order_idx = 0
 
+    index = music_orders_str.find(memory_orders)
 
-        # global_orders와 music_name_to_orders의 각 name에 해당하는 부분이 몇개인지 비교
+    if index == -1:
+        return "(None)"
+
+    result_map = {}
+    for i in range(index, index + len(memory_orders)):
+        music_name_key = music_order_list[i][1]
+        if music_name_key in result_map:
+            result_map[music_name_key] += 1
+        else:
+            result_map[music_name_key] = 1
+
+    max = 0
+    for key in result_map:
+        if max < result_map[key]:
+            max = result_map[key]
+            answer = key
 
     return answer
 
 
-def get_time_to_minute(time):
+def time_to_long(time):
     split = time.split(":")
     hour = int(split[0])
     minute = int(split[1])
     return hour * 60 + minute
 
 
-def get_list_order(order):
-    result = []
-    index = 0
-    while index < len(order):
-        if index + 1 < len(order) and order[index + 1] == '#':
-            result.append(order[index] + order[index + 1])
-            index += 1
-        else:
-            result.append(order[index])
-        index += 1
-
-    return result
+def replace_orders(orders):
+    return orders.replace('C#', 'c').replace('D#', 'd').replace('F#', 'f').replace('G#', 'g').replace('A#', 'a')
 
 
-m = "CC#BCC#BCC#BCC#B"
-musicionfos = ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]
-print(solution(m, musicionfos))
+# m = "ABCDEFG"
+# musicinfos = ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]
+# print(solution(m, musicinfos))
+#
+# m1 = "CC#BCC#BCC#BCC#B"
+# musicinfos1 = ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]
+# print(solution(m1, musicinfos1))
+
+m2 = "ABC"
+musicinfos2 = ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]
+print(solution(m2, musicinfos2))
+
+print(solution("C", ["13:00,13:01,WORLD,F"]))
