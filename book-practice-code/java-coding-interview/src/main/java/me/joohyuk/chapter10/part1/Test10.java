8d73dffe2e304d7e6270bfc5e1cca611b4761634
joohyuk2074 <vfrvfr4207@gmail.com>
2023-09-23 22:44:53 +0900

    feat: 코딩 테스트 10: 0을 포함하는 행렬

---
+++ book-practice-code/java-coding-interview/src/main/java/me/joohyuk/chapter10/part1/Test10.java
@@ -0,0 +1,77 @@
+package me.joohyuk.chapter10.part1;
+
+public class Test10 {
+
+    public static void alignZeros(int[][] m) {
+        if (m == null || m.length == 0) {
+            throw new IllegalArgumentException("The given matrix cannot be null or empty.");
+        }
+
+        boolean firstRowHasZeros = false;
+        boolean firstColumnHasZeros = false;
+
+        // 첫 번째 행에 0이 하나 이상 있는지 확인합니다.
+        for (int j = 0; j < m[0].length; j++) {
+            if (m[0][j] == 0) {
+                firstRowHasZeros = true;
+                break;
+            }
+        }
+
+        // 첫 번째 열에 0이 하나 이상 있는지 확인합니다.
+        for (int i = 0; j < m.length; i++) {
+            if (m[i][0] == 0) {
+                firstColumnHasZeros = true;
+                break;
+            }
+        }
+
+        // 행렬의 나머지 영역에 있는 모든 0을 찾습니다.
+        for (int i = 1; i < m.length; i++) {
+            for (int j = 1; j < m[0].length; j++) {
+                if (m[i][j] == 0) {
+                    m[i][0] = 0;
+                    m[0][j] = 0;
+                }
+            }
+        }
+
+        // 첫 번째 열을 순회한 후 값이 0인 행을 찾아 해당 행을 모두 0으로 설정합니다.
+        for (int i = 1; i < m.length; i++) {
+            if (m[i][0] == 0) {
+                setRowOfZero(m, i);
+            }
+        }
+
+        // 첫 번째 행을 순회한 후 값이 0인 열을 찾아 해당 열을 모두 0으로 설정합니다.
+        for (int j = 1; j < m[0].length; j++) {
+            if (m[0][j] == 0) {
+                setColumnOfZero(m, j);
+            }
+        }
+
+        if (firstRowHasZeros) {
+            setRowOfZero(m, 0);
+        }
+
+        if (firstColumnHasZeros) {
+            setColumnOfZero(m, 0);
+        }
+    }
+
+    private static void setRowOfZero(int[][] m, int r) {
+        for (int j = 0; j < m[0].length; j++) {
+            m[r][j] = 0;
+        }
+    }
+
+    private static void setColumnOfZero(int[][] m, int c) {
+        for (int i = 0; i < m.length; i++) {
+            m[i][c] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
