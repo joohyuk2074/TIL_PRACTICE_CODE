7e1f4e0409e65b7d4a84446a8d1699813b5ef62c
joohyuk2074 <vfrvfr4207@gmail.com>
2023-09-28 17:31:18 +0900

    feat: 1520풀이 완료

---
+++ python-algorithm/baekjoon/최적화 ( 재귀, 백트래킹의 경우의 수 )/1520(풀이완료).py
@@ -0,0 +1,24 @@
+def recursive(x, y):
+    if x == m - 1 and y == n - 1:
+        return 1
+
+    if dp[x][y] != -1:
+        return dp[x][y]
+
+    route = 0
+
+    for dx, dy in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+        nx = x + dx
+        ny = y + dy
+        if 0 <= nx < m and 0 <= ny < n:
+            if graph[x][y] > graph[nx][ny]:
+                route += recursive(nx, ny)
+
+    dp[x][y] = route
+    return dp[x][y]
+
+
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(m)]
+dp = [[-1 for _ in range(n)] for _ in range(m)]
+print(recursive(0, 0))
