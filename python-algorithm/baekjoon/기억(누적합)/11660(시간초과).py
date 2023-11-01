fdf49b3043de63e880385719b907ce11ba24058d
joohyuk2074 <vfrvfr4207@gmail.com>
2023-11-01 23:21:27 +0900

    feat: 백준 11660 풀이

---
+++ python-algorithm/baekjoon/기억(누적합)/11660(시간초과).py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+prefix = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+for x in range(n):
+    for y in range(n):
+        prefix[x + 1][y + 1] = prefix[x][y + 1] + prefix[x + 1][y] - prefix[x][y] + graph[x][y]
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    answer = prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1]
+    print(answer)
